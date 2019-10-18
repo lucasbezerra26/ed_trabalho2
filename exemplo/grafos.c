942ceea4b3764b070a7ee14f7bc6a6fbbd979667
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2019-10-18 15:20:54 -0300

    adicionado cidades

--- exemplo/grafos.c
+++ exemplo/grafos.c
@@ -8,16 +8,18 @@ struct Grafo{
 	int** aresta;
 	float** pesos;
 	int* grau;
+	float valor_total;
 };
 
 typedef struct Grafo grafo;
 
-grafo *cria_grafo(int nro_vertices, int grau_max, int eh_ponderado){
+grafo *cria_grafo(int nro_vertices, int grau_max, int eh_ponderado, float valor_total){
 	grafo *gr = (grafo*) malloc(sizeof(grafo));
 	if ( gr != NULL){
 	 	int i;
 	 	gr->nro_vertices = nro_vertices;
 	 	gr->grau_max = grau_max;
+		gr->valor_total = valor_total;
 	 	gr->eh_ponderado = (eh_ponderado !=0 )?1:0;//só para garanatir que vai ser 0 ou 1
 	 	gr->grau = (int*) calloc(nro_vertices,sizeof(int*)); //criando a lista
 	 	for (int i = 0; i < nro_vertices; i++){
@@ -73,11 +75,31 @@ int main(int argc, char const *argv[]){
 
 	grafo *g = NULL;
 	int num_cidades;
+	float caixa;
 
 	printf("Digite a quantidade de cidades: ");
 	scanf(" %d", &num_cidades);
-	g = cria_grafo(num_cidades, num_cidades, 1);
-
 	
+	printf("Qual o valor máximo para gasto: ");
+	scanf(" %f", &caixa);
+
+	g = cria_grafo(num_cidades, num_cidades, 1, caixa);
+
+	printf("Suas cidades são numeradas entre 1 e %d\n", num_cidades);
+
+	int status = 1;
+	int cidade_a, cidade_b;
+	float peso;
+
+	while (status){
+		printf("Digite as rodovias que ligam a cidade. \nEx.: 1 5 ligam as cidades 1 e 5 ida e volta\n");
+		scanf(" %d %d", &cidade_a, &cidade_b);
+		printf("Qual o valor do pedágio? ");
+		scanf(" %f", &peso);
+		insereAresta(g, cidade_a, cidade_b, 0, peso);
+		printf("Deseja inserir mais? 1-sim/2-nao \n");
+		scanf(" %d", &status);	
+	};
+
 	return 0;
 }
