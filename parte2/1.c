8f62671a0801b6e198c1e66df5a0e0ecb6ef5f5d
lucasbezerra26 <lucasbezerra527@gmail.com>
2019-10-23 22:17:13 -0300

    versao dando certo com entrada setada no condigo

--- parte2/1.c
+++ parte2/1.c
@@ -228,7 +228,7 @@ int main(){
 	insereAresta(g, 1, 2, 0, 1);
 	insereAresta(g, 2, 3, 0, 2);
 	insereAresta(g, 2, 4, 0, 1);
-
+ 
 	// insereAresta(g, 1, 5, 0, 1);
 	// insereAresta(g, 1, 2, 0, 1);
 
